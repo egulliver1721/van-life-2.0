e5a5dba48d2c168adcbf0ea67100443f8536b62f
egulliver1721 <118499423+egulliver1721@users.noreply.github.com>
2023-04-15 12:31:46 +0800

    added dynamic route with van id

--- frontend/src/pages/VanDetail.tsx
+++ frontend/src/pages/VanDetail.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 
 const instance = axios.create({
   baseURL: "http://localhost:8080",
+  withCredentials: true,
 });
 
 type Van = {
@@ -39,12 +40,27 @@ const VanDetail = () => {
 
   return (
     <div>
-      <img src={van.imageUrl} alt="" />
-      <h1>{van.name}</h1>
-      <p>
-        {van.price} <span>/day</span>
-      </p>
-      <p>{van.description}</p>
+      <Link to="/vans" className="back">
+        back to vans
+      </Link>
+      <div className="van-detail-container">
+        {van ? (
+          <div className="van-detail">
+            <img src={van.imageUrl} />
+            <i className={`van-type ${van.type} selected centered`}>
+              {van.type}
+            </i>
+            <h2>{van.name}</h2>
+            <p className="van-price">
+              <span>${van.price}</span>/day
+            </p>
+            <p>{van.description}</p>
+            <button className="link-button">Rent this van</button>
+          </div>
+        ) : (
+          <h2>Loading...</h2>
+        )}
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Vans.tsx
+++ frontend/src/pages/Vans.tsx
@@ -34,7 +34,7 @@ const Home = () => {
 
   const vanElements = vans.map((van) => (
     <div key={van.id} className="van-tile">
-      <Link to={`/vans/:${van.id}`}>
+      <Link to={`/vans/${van.id}`}>
         <img src={van.imageUrl} />
         <div className="van-info">
           <h3>{van.name}</h3>
